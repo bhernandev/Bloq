caf5d583e14480c57000e6369eaafe8086e27c4f
Brian Hernandez <Brian@Hernandez-cs.com>
2016-11-28 11:55:22 -0500

    Collision testing added

--- Bloq/GameScene.swift
+++ Bloq/GameScene.swift
@@ -9,6 +9,15 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    func makeBouncer(at position: CGPoint){
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width/2.0)
+        bouncer.physicsBody!.isDynamic = false
+        addChild(bouncer)
+    }
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background.jpg")
         background.position = CGPoint(x: 512, y: 384)
@@ -16,6 +25,11 @@ class GameScene: SKScene {
         background.zPosition = -1
         addChild(background)
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        makeBouncer(at: CGPoint(x: 0, y:100))
+        makeBouncer(at: CGPoint(x: 256, y:100))
+        makeBouncer(at: CGPoint(x: 512, y:100))
+        makeBouncer(at: CGPoint(x: 768, y:100))
+        makeBouncer(at: CGPoint(x: 1024, y:100))
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
